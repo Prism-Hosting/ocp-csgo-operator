dc406710eeb5bea0c8cc6eeb70c93a4b67c9176c
The Man <msblauwal6@gmail.com>
2023-05-06 21:00:49 +0200

    [Test] add supervise_ips()

--- operator/modules/forwarder.py
+++ operator/modules/forwarder.py
@@ -5,6 +5,7 @@ Module to automatically forwards ports to CS:GO services on a UDM SE.
 import kopf
 import kubernetes
 import logging
+import json
 import os
 import uuid
 import kopf
@@ -57,8 +58,9 @@ def do_unifi_request(mode, target_url, json=None, cookies=None, csrf_token=None)
         
         if mode == "get":            
             response = session.get(target_url, headers=request_headers, json=json, cookies=cookies, verify=False)
-            
         elif mode == "post":
+            response = session.post(target_url, headers=request_headers, json=json, cookies=cookies, verify=False)
+        elif mode == "delete":
             response = session.post(target_url, headers=request_headers, json=json, cookies=cookies, verify=False)
             
         else:
@@ -124,8 +126,8 @@ def create_port_forward(target_ip, target_port):
     """ Create UDM SE/PRO port forwarding rule
 
     Args:
-        target_ip (string): IP of host to forward to
-        target_port (string): Port (source and dest) to forward
+        target_ip (string): IP of host to forward to.
+        target_port (string): Port (source and dest) to forward.
     """
     
     target_url = f"https://{os.environ['UNIFI_API_HOST']}/proxy/network/api/s/default/rest/portforward"
@@ -139,6 +141,27 @@ def create_port_forward(target_ip, target_port):
         
     except Exception as e:
         print(f"Failed: {str(e)}")
+
+def delete_port_forward(id):
+    """ Delete UDM SE/PRO port forwarding rule
+
+    Args:
+        target_ip (string): ID of port forward object
+    """
+    
+    target_url = f"https://{os.environ['UNIFI_API_HOST']}/proxy/network/api/s/default/rest/portforward/{id}"
+    
+    try:
+        response = do_unifi_request("delete", target_url)
+        
+        if not response.status_code == 200:
+            raise ValueError(f"Status code is {str(response.status_code)} - {response.text}")
+        
+    except Exception as e:
+        print(f"Failed: {str(e)}")
+    
+    
+    return True
         
 def get_port_forward():
     """ Get UDM SE/PRO port forwarding rules
@@ -165,6 +188,46 @@ def get_port_forward():
 #           LOGIC
 #  ------------------------
 
+def supervise_ips():
+    try:
+        client = kube_auth()
+        
+        api = client.resources.get(api_version="v1", kind="Service")
+        items = api.get(namespace="prism-servers", label_selector="customer").items
+        
+        forwards = get_port_forward().data
+        
+        for item in items:
+            do_delete = False
+            
+            # Only proceed if loadBalancer has assigned an IP
+            if item.status.loadBalancer:
+                this_ip = item.status.loadBalancer.ingress[0].ip
+                this_port = item.spec.ports[0].port
+                
+                for obj in json.loads(forwards):
+                    if obj["fwd"] == this_ip:
+                        if obj["fwd_port"] == this_port:
+                            print("Forward found and it is correct.")
+                        else:
+                            print(f"Correcting forward for {this_ip}...")
+                            do_delete = True
+                            
+                    else:
+                        print(f"No forward for {this_ip}...")
+                        do_delete = True
+                
+            # (Re)create port forward
+            if do_delete:
+                print(f"> Creating forward for: {this_port} -> {this_ip}")
+                delete_port_forward(obj["_id"])
+                delete_port_forward(this_ip, this_port)
+                
+            else:
+                print("No external IP for this item.")
+        
+    except Exception as e:
+        raise kopf.TemporaryError(f"FORWARDER: Error during supervision: {str(e)}")
 
 """
 Sample request:

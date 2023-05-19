f368efd1ad7c17b5a054ede251622819540879e4
The Man <msblauwal6@gmail.com>
2023-05-19 17:26:17 +0200

    Improve flexibility of patch_resource()

--- operator/modules/utils.py
+++ operator/modules/utils.py
@@ -18,19 +18,21 @@ def kube_auth():
     except Exception as e:
         raise kopf.PermanentError(f"Failed to create dynamic client: {str(e)}")
     
-def patch_resource(resource_name, body):
-    """ Patch a PrismServer resource
+def patch_resource(resource_name, body, kind="PrismServer", namespace="prism-servers"):
+    """ Patch a kubernetes resource, defaults to "PrismServer"
 
     Args:
         resource_name (string): Name of the PrismServer resource (meta.name)
         body (dict): Body to patch resource with
+        kind (string): Target kind
+        namespace (string): Target namespace
     """
     
     client = kube_auth()
     
-    prismserver_api = client.resources.get(api_version="v1", kind="PrismServer")
+    prismserver_api = client.resources.get(api_version="v1", kind=kind)
     prismserver_api.patch(
-        namespace="prism-servers",
+        namespace=namespace,
         name=resource_name,
         body=body,
         content_type="application/merge-patch+json"
